ff9ab987842295964b2be309fcb5acb3819ceb2d
Pablo Roldão <pablo.roldao.santos@gmail.com>
2023-06-18 22:51:20 -0300

    Basis PHP syntax...

--- index.php
+++ index.php
@@ -1,1 +1,28 @@
-Hello World!
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Document</title>
+</head>
+
+<body>
+    <h1>
+        <?= 'Hello Word!' ?>
+    </h1>
+    <p>My first paragraph.</p>
+    <?php
+        //comment 1
+        #comment 2
+        /*
+            comment 3
+        */
+
+        $x = 10;
+        $y = 5;
+
+        echo '<p>' . $x . ', ' . $y . '</p>'
+    #comment?>
+</body>
+
+</html>
